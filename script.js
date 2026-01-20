cb5d5b402aec1853dc9ffac4c64e717d174f6247
LovelyChrist <dorneaujean@gmail.com>
2026-01-20 06:59:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function showMessage() {
     document.getElementById("message").innerText =
         "Hello! Your JavaScript is working 👍";
 }
+
+/* Slideshow Logic */
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[currentSlide].classList.add("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+}
+
+setInterval(showSlides, 3000);
